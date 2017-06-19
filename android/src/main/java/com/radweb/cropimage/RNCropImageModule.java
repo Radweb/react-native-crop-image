0f25170f746d6d4950b86ae893ae5aeae345d238
Louis Capitanchik <contact@louiscap.co>
2017-06-19 13:19:13 +0100

    Crop image on android

--- android/src/main/java/com/radweb/cropimage/RNCropImageModule.java
+++ android/src/main/java/com/radweb/cropimage/RNCropImageModule.java
@@ -1,22 +1,71 @@
-
 package com.radweb.cropimage;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Promise;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class RNCropImageModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+	private final ReactApplicationContext reactContext;
 
-  public RNCropImageModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+	public RNCropImageModule(ReactApplicationContext reactContext) {
+		super(reactContext);
+		this.reactContext = reactContext;
+	}
 
-  @Override
-  public String getName() {
-    return "RNCropImage";
-  }
+	@Override
+	public String getName() {
+		return "RNCropImage";
+	}
+
+	@ReactMethod
+	public void crop(String path, ReadableMap options, Promise promise) {
+		BitmapFactory factory = new BitmapFactory();
+		Bitmap image = factory.decodeFile(path);
+		Bitmap cropped = Bitmap.createBitmap(
+				image,
+				options.getInt("left"),
+				options.getInt("top"),
+				options.getInt("width"),
+				options.getInt("height")
+		);
+
+		FileOutputStream out = null;
+		File dest = getOutputPath();
+
+		try {
+			out = new FileOutputStream(dest);
+			cropped.compress(Bitmap.CompressFormat.JPEG, 100, out);
+			promise.resolve(dest.getAbsolutePath());
+		} catch (Exception e) {
+				promise.reject("Failed to save", "Failed to save to file", null);
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				promise.reject("Failed to save", "Failed to close saved file stream", null);
+			}
+		}
+	}
+
+	private File getOutputPath() {
+		File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+		String fileName = String.format("IMG_%s.jpg", new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()));
+		return new File(String.format("%s%s%s", root.getPath(), File.separator, fileName));
+	}
 }
